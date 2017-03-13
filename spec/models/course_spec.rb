9d19f86873a7e06ec5ab5b51f33ec517b0617220
Yining Bao <geminiabao@gmail.com>
2017-03-13 17:56:11 +0000

    Course_spec

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe Course, type: :model do
     it { is_expected.to respond_to :number }
     it { is_expected.to respond_to :name }
     it { is_expected.to respond_to :credit }
-    it { is_expected.to respond_to :prerequisite }
+    it { is_expected.to respond_to :description }
+    it { is_expected.to respond_to :is_fall }
+    it { is_expected.to respond_to :is_spring }
+    it { is_expected.to respond_to :is_summer }
+
 end
