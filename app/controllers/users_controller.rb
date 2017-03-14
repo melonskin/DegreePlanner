8cda79d0bd911833141ebadd546bbc49e2ee31b2
melonskin <tsjtux@gmail.com>
2017-03-14 16:20:44 -0500

    finish sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
+    # .authenticate(params[:id][:email])
     # debugger
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Degree Planner!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,:password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   root 'students#index'
   resources :students
 end
