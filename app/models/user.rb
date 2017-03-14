25febd599d7d2f7e93207673966134b6c09c424c
Johana Rueda <johana.m.rueda@tamu.edu>
2017-03-14 15:47:00 +0000

    New user model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+   attr_accessor :name, :email
+    
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 }
+    
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  #get 'users/new'
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'students#index'
   resources :students
