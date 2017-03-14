d65f2c29f1c712c6d8f61ed30852134610f8a5c8
melonskin <tsjtux@gmail.com>
2017-03-14 12:12:19 -0500

    sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  '/signup',  to: 'users#new'
   root 'students#index'
   resources :students
 end
