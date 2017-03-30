6024d6779b77ce937c12aed1ea7116599af712d2
Yining Bao <geminiabao@gmail.com>
2017-03-30 23:08:39 +0000

    Validate logged in before seeing the information of the user & corresponding rspec

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,4 +20,15 @@ module SessionsHelper
     @current_student ||= Student.find_by(user_id: session[:user_id])
   end
   
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+  
 end
