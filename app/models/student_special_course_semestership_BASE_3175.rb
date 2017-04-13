788efd0e9afe1ae8e352cffcfe6043d1023d84cf
Yining Bao <geminiabao@gmail.com>
2017-04-13 21:56:06 +0000

    fix conflict

--- app/models/student_special_course_semestership_BASE_3175.rb
+++
@@ -1,9 +0,0 @@
-class StudentSpecialCourseSemestership < ApplicationRecord
-    belongs_to :student
-    belongs_to :special_course
-    belongs_to :semester
-    
-    before_save
-    validates :special_course, uniqueness: true
-    # { scope: :semester, message: "should happen once per semester" }
-end
