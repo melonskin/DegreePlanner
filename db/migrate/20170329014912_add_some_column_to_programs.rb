71c79344e1e0d6104f4591301b4648fccbf095a3
melonskin <tsjtux@gmail.com>
2017-04-23 15:19:21 -0500

    modify program and add seed data

--- db/migrate/20170329014912_add_some_column_to_programs.rb
+++ db/migrate/20170329014912_add_some_column_to_programs.rb
@@ -2,7 +2,9 @@ class AddSomeColumnToPrograms < ActiveRecord::Migration[5.0]
   def change
     add_column :programs, :dep_hour, :integer
     add_column :programs, :graded_grad_hour, :integer
-    add_column :programs, :ug_class, :integer
+    add_column :programs, :ug_hour_min, :integer
+    add_column :programs, :ug_hour_max, :integer
+
     add_column :programs, :non_dep_hour_min, :integer
     add_column :programs, :non_dep_hour_max, :integer
     add_column :programs, :seminar_hour_min, :integer
@@ -17,5 +19,6 @@ class AddSomeColumnToPrograms < ActiveRecord::Migration[5.0]
     add_column :programs, :joint_hour_max, :integer
     add_column :programs, :elective_hour_min, :integer
     add_column :programs, :elective_hour_max, :integer
+    add_column :programs, :joint_dep, :string
   end
 end
